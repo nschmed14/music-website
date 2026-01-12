8f1b62ecfaf031f1e3c469128ac202ac64f89231
nschmed14 <87058432+nschmed14@users.noreply.github.com>
2026-01-12 01:07:34 +0000

    Fix YouTube iframe embeds - add missing src attribute

--- src/components/Media.js
+++ src/components/Media.js
@@ -105,6 +105,7 @@ const Media = () => {
                 <div className="w-full lg:w-2/3 relative">
                   <div className="relative" style={{ paddingBottom: '56.25%' }}>
                     <iframe
+                      src={`https://www.youtube-nocookie.com/embed/${video.id}`}
                       className="absolute top-0 left-0 w-full h-full"
                       title={`${video.title} - ${video.composer}`}
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
